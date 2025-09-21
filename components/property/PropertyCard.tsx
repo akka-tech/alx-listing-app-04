b2fd9d419421889d9ae9e4c95dd4d36dec279197
akka-tech <akkaabdlwahid3@gmail.com>
2025-09-21 14:36:07 +0200

    add

---
+++ components/property/PropertyCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export interface Property {
+  id: string | number;
+}
+function PropertyCard({property}: any) {
+  return (
+    <div>PropertyCard</div>
+  )
+}
+
+export default PropertyCard
